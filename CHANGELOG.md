305eb034ca077a9b047c003892cf46c8d6e8b729
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2020-11-23 19:07:01 +0300

    clear

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## Version 1.1 (September 22, 2016)
-
-Теперь использую шаблонизатор Pug(Jade)
--- src/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).foundation();
-$(document).ready(function(){
-	console.log('test console');
-});
-
-
